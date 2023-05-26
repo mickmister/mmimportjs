3c3fab62baadb56866ff567fb0ff6b98abe39a67
Michael Kochell <6913320+mickmister@users.noreply.github.com>
2023-05-26 19:04:36 -0400

    avoid creating extra file

--- src/chunk-posts.ts
+++ src/chunk-posts.ts
@@ -1,72 +1,74 @@
 import fs from 'node:fs';
 import readline from 'node:readline';
 
-const inputFileName = process.argv[2];
+export const runChunkPosts = (inputFileName: string) => {
+    let beginTimeString = new Date().toISOString();
+    beginTimeString = beginTimeString.substring(0, beginTimeString.length - 5);
+    const outFolder = `out/${beginTimeString}`;
+    fs.mkdirSync(outFolder, {recursive: true});
 
-if (!inputFileName) {
-    console.log('Please provide the import file as an argument. For example:');
-    console.log('chunk-posts myimport.jsonl')
-    process.exit(0);
-}
-
-if (!inputFileName.endsWith('.jsonl')) {
-    console.log('Please provide a valid import file that has extension .jsonl');
-    process.exit(0);
-}
-
-let beginTimeString = new Date().toISOString();
-beginTimeString = beginTimeString.substring(0, beginTimeString.length - 5);
-const outFolder = `out/${beginTimeString}`;
-fs.mkdirSync(outFolder, {recursive: true});
-
-let CHUNK_SIZE = 10000;
-const providedChunkSize = process.argv[3];
-if (providedChunkSize) {
-    CHUNK_SIZE = parseInt(providedChunkSize);
-}
-
-const makeWriteStream = (): fs.WriteStream => {
-    const fileNumString = currentFileNum.toString().padStart(6, '0');
-    const outputFileName = `${outFolder}/${fileNumString}-${inputFileName}`;
-    return fs.createWriteStream(outputFileName, {autoClose: true});
-};
-
-const rotateFile = () => {
-    writeStream.close();
-    currentFileNum++;
-    writeStream = makeWriteStream();
-    writeStream.write(versionLine + '\n');
-};
-
-console.log(`Creating chunks, with ${CHUNK_SIZE} lines per chunk...`);
-
-const versionLine = '{"type":"version","version":1}';
-
-let currentFileNum = 1;
-let currentLineNum = -1;
-
-let writeStream = makeWriteStream();
-
-const rl = readline.createInterface({
-    input: fs.createReadStream(inputFileName),
-    crlfDelay: Infinity,
-});
-
-rl.on('line', (line: string) => {
-    currentLineNum++;
-
-    writeStream.write(line + '\n');
-
-    if (currentLineNum !== 0 && currentLineNum % CHUNK_SIZE === 0) {
-        console.log(`Created chunk ${currentFileNum}. Processed ${currentLineNum} lines total.`);
-        rotateFile();
+    let CHUNK_SIZE = 10000;
+    const providedChunkSize = process.argv[3];
+    if (providedChunkSize) {
+        CHUNK_SIZE = parseInt(providedChunkSize);
     }
-});
 
-rl.on('close', () => {
-    if (currentLineNum % CHUNK_SIZE !== 0) {
+    const getCurrentFileName = () => {
+        const fileNumString = currentFileNum.toString().padStart(6, '0');
+        return `${outFolder}/${fileNumString}-${inputFileName}`;
+    };
+
+    const makeWriteStream = (): fs.WriteStream => {
+        const outputFileName = getCurrentFileName();
+        return fs.createWriteStream(outputFileName, {autoClose: true});
+    };
+
+    const deleteLastFile = () => {
+        const outputFileName = getCurrentFileName();
+        fs.rmSync(outputFileName);
+    };
+
+    const rotateFile = () => {
         writeStream.close();
-        console.log(`Created chunk ${currentFileNum}. Processed ${currentLineNum} lines total.`);
+        currentFileNum++;
+        writeStream = makeWriteStream();
+        writeStream.write(versionLine + '\n');
+    };
+
+    console.log(`Creating chunks, with ${CHUNK_SIZE} lines per chunk...`);
+
+    const versionLine = '{"type":"version","version":1}';
+
+    let currentFileNum = 1;
+    let currentLineNum = -1;
+
+    let writeStream = makeWriteStream();
+
+    const rl = readline.createInterface({
+        input: fs.createReadStream(inputFileName),
+        crlfDelay: Infinity,
+    });
+
+    rl.on('line', (line: string) => {
+        if (!line) {
+            return;
+        }
+
+        currentLineNum++;
+        writeStream.write(line + '\n');
+
+        if (currentLineNum !== 0 && currentLineNum % CHUNK_SIZE === 0) {
+            console.log(`Created chunk ${currentFileNum}. Processed ${currentLineNum} lines total.`);
+            rotateFile();
+        }
+    });
+
+    rl.on('close', () => {
+        if (currentLineNum % CHUNK_SIZE !== 0) {
+            console.log(`Created chunk ${currentFileNum}. Processed ${currentLineNum} lines total.`);
+        } else {
+            deleteLastFile();
+        }
         console.log('Finished.');
-    }
-});
+    });
+};
